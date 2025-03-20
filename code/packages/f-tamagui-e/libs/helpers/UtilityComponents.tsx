e5b45851c22837ec7b216900b0fbb5f4e1557c39
timur x <timurx@eezza.dev>
2025-03-20 05:41:09 +0100

    tamagui/UtilityComponents

---
+++ code/packages/f-tamagui-e/libs/helpers/UtilityComponents.tsx
@@ -0,0 +1,24 @@
+// import type { ViewStyle } from 'tamagui'
+import { View, styled, isWeb } from 'tamagui'
+import { _feIsString } from '@mflt/_fe'
+
+export const ViewwithDisplayContents = styled(View,
+  isWeb
+  ? {
+      display: 'contents',
+    }
+  : {}  // @TODO is it the rite choice here?
+)
+
+export function getNamedCssGridAreaasView (
+  areaName: string
+) {
+  if (!_feIsString(areaName) || !isWeb) {
+    // @TODO error message
+    return null
+  }
+  return styled(View, {
+    // display: 'contents',
+    '$platform-web': {gridArea: areaName}
+  })
+}
--- code/packages/f-tamagui-e/libs/index.ts
+++ code/packages/f-tamagui-e/libs/index.ts
@@ -1,1 +1,2 @@
 export * from './components/BlocksSlider.js'
+export * from './helpers/index.js'
